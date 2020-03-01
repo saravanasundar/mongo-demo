e17c87f04d75631e0ca32f0967820a33a0bad1e8
saravana <41249153+saravanasakthi@users.noreply.github.com>
2020-03-01 18:47:59 +0530

    Mongo-NodeApi Final

--- index.js
+++ index.js
@@ -6,29 +6,43 @@ mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
 
 const courseSchema = new mongoose.Schema({
     name: {type: String, required: true, minlength: 5, maxlength: 20},
-    category: {type: String, enum: ['web', 'mobile', 'network'], required: true}, 
+    category: {type: String, enum: ['web', 'mobile', 'network'], required: true, lowercase: true, trim: true}, 
     author: String,
     // tags: [String],
+    //Custom Validation
+    // tags: {type: Array, validate:{
+    //     validator: function(v){
+    //         return v && v.length > 0;
+    //     },
+    //     message: 'A course should have atleast one tag'
+    // }},
+
+    //Async Validation
     tags: {type: Array, validate:{
-        validator: function(v){
-            return v && v.length > 0;
+        isAsync: true,
+        validator: function(v, cb){
+            setTimeout(()=>{
+                const result = v && v.length > 0;
+                cb(result)
+            }, 4000)            
         },
         message: 'A course should have atleast one tag'
     }},
+
     date: {type: Date, default: Date.now},
     isPublished: Boolean,
-    price: {type: Number, required: function(){return this.isPublished;}, min: 10, max: 200} //price required if the ispublished is true
+    price: {type: Number, required: function(){return this.isPublished;}, min: 10, max: 200, get: v=>Math.round(v), set: v=>Math.round(v)} //price required if the ispublished is true
 });
 const Course = mongoose.model('Course', courseSchema); //Class
 
 async function createCourse(){
     const course = new Course({
         name: 'Node js course',
-        category: 'web',
+        category: 'WEB',
         author: 'Mosh Hamedani',
-        tags: ['Node', 'Backend'],
+        tags: ['Backend'],
         isPublished: true,
-        price: 25
+        price: 25.2525
     });
 
     try{
@@ -42,7 +56,10 @@ async function createCourse(){
         const result = await course.save();
         console.log(result)
     }catch(ex){
-        console.log(ex.message)
+        // console.log(ex.message) //normal validation
+        
+        //Validation errors
+        for( field in ex.errors) console.log(ex.errors[field].message)
     }    
 }
 
